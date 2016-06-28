bda5669fdba54f9626001cb280b2df775d4d5f16
root <root@kogo>
2016-06-28 09:03:43 +0200

    Saving status

--- app/src/main/java/edu/fci/smartcornea/Constant.java
+++ app/src/main/java/edu/fci/smartcornea/Constant.java
@@ -13,5 +13,5 @@ public abstract class Constant {
     public static final Integer LBPH_GRID_X = 8;
     public static final Integer LBPH_GRID_Y = 8;
     public static final Double LBPH_THRESHOLD = 180.0;
-    public static final Size LBPH_FaceSize = new Size(150, 150);
+    public static final Size LBPH_FaceSize = new Size(92, 112);
 }
